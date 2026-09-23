077f1ab51540db3d89afa87298a2c6d7ae6621b9
tachsin <tachsinachmet@gmail.com>
2026-09-23 17:15:00 +0300

    Fix infinite loop in Range/MultiRange permutation when a step does not advance
    The Step, StepScaled and Discrete value iterations (used for permutation
    and chromosome_permutations_size, the latter also via Display) looped
    until value >= end. When the step does not advance the value this never
    happens:
    - Step(0.0) or a zero in StepScaled (accepted by the builder)
    - a float step below the precision of the value (f32 1500.0 + 1e-5)
    - Discrete above 2^24 for f32
    
    Extract the 12 identical iterations into a step_values helper which jumps
    to the end when the step does not advance the value. Results for advancing
    steps are unchanged.

--- src/genotype/multi_range.rs
+++ src/genotype/multi_range.rs
@@ -1,4 +1,5 @@
 use super::builder::{Builder, TryFromBuilderError};
+use super::range::step_values;
 use super::{
     EvolveGenotype, Genotype, HillClimbGenotype, MutationType, PermutateGenotype,
     SupportsGeneCrossover, SupportsPointCrossover,
@@ -828,19 +829,7 @@ where
     pub fn permutable_gene_values_step(&self, index: usize, step: T) -> Vec<T> {
         let allele_range_start = *self.allele_ranges[index].start();
         let allele_range_end = *self.allele_ranges[index].end();
-        std::iter::successors(Some(allele_range_start), |value| {
-            if *value < allele_range_end {
-                let next_value = *value + step;
-                if next_value > allele_range_end {
-                    Some(allele_range_end)
-                } else {
-                    Some(next_value)
-                }
-            } else {
-                None
-            }
-        })
-        .collect()
+        step_values(allele_range_start, allele_range_end, step).collect()
     }
     pub fn permutable_gene_values_step_scaled(
         &self,
@@ -865,19 +854,7 @@ where
         };
 
         let working_step = steps[self.current_scale_index];
-        std::iter::successors(Some(allele_value_start), |value| {
-            if *value < allele_value_end {
-                let next_value = *value + working_step;
-                if next_value > allele_value_end {
-                    Some(allele_value_end)
-                } else {
-                    Some(next_value)
-                }
-            } else {
-                None
-            }
-        })
-        .collect()
+        step_values(allele_value_start, allele_value_end, working_step).collect()
     }
 
     pub fn permutable_gene_values_discrete(
@@ -887,19 +864,7 @@ where
     ) -> Vec<T> {
         let allele_value_start = self.allele_ranges[index].start().floor();
         let allele_value_end = self.allele_ranges[index].end().floor();
-        std::iter::successors(Some(allele_value_start), |value| {
-            if *value < allele_value_end {
-                let next_value = *value + T::one();
-                if next_value > allele_value_end {
-                    Some(allele_value_end)
-                } else {
-                    Some(next_value)
-                }
-            } else {
-                None
-            }
-        })
-        .collect()
+        step_values(allele_value_start, allele_value_end, T::one()).collect()
     }
 
     pub fn chromosome_permutations_size_per_scale(&self) -> Vec<BigUint> {
@@ -927,19 +892,7 @@ where
                 MutationType::Step(step) => {
                     let allele_value_start = *self.allele_ranges[index].start();
                     let allele_value_end = *self.allele_ranges[index].end();
-                    std::iter::successors(Some(allele_value_start), |value| {
-                        if *value < allele_value_end {
-                            let next_value = *value + *step;
-                            if next_value > allele_value_end {
-                                Some(allele_value_end)
-                            } else {
-                                Some(next_value)
-                            }
-                        } else {
-                            None
-                        }
-                    })
-                    .count()
+                    step_values(allele_value_start, allele_value_end, *step).count()
                 }
                 MutationType::StepScaled(steps) => {
                     let (allele_value_start, allele_value_end) =
@@ -954,37 +907,13 @@ where
                         };
 
                     let working_step = steps[scale_index];
-                    std::iter::successors(Some(allele_value_start), |value| {
-                        if *value < allele_value_end {
-                            let next_value = *value + working_step;
-                            if next_value > allele_value_end {
-                                Some(allele_value_end)
-                            } else {
-                                Some(next_value)
-                            }
-                        } else {
-                            None
-                        }
-                    })
-                    .count()
+                    step_values(allele_value_start, allele_value_end, working_step).count()
                 }
                 MutationType::Discrete => {
                     let allele_value_start = self.allele_ranges[index].start().floor();
                     let allele_value_end = self.allele_ranges[index].end().floor();
 
-                    std::iter::successors(Some(allele_value_start), |value| {
-                        if *value < allele_value_end {
-                            let next_value = *value + T::one();
-                            if next_value > allele_value_end {
-                                Some(allele_value_end)
-                            } else {
-                                Some(next_value)
-                            }
-                        } else {
-                            None
-                        }
-                    })
-                    .count()
+                    step_values(allele_value_start, allele_value_end, T::one()).count()
                 }
                 _ => {
                     panic!(
